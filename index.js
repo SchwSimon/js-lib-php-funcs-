eb3cce23b3b294bae53884307a8f56ff5ae78b0e
Simon <SchwSimon@users.noreply.github.com>
2017-09-09 16:50:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+ * somestuff
+ * by Simon
+ */
+
+'use strict';
+
+exports.str = require( './lib/String' );
+exports.math = require( './lib/Math' );
+exports.arr = require( './lib/Array' );
+exports.files = require( './lib/Files' );
